7c2cc56e7d637c2f101eaa870369cedc59f425ce
Alok <rohan.arya.786@gmail.com>
2017-02-26 15:41:09 +0530

    OkHttp

--- app/src/main/java/com/example/babu/jobsandesh/jobseeker/JobseekerLoginActivity.java
+++ app/src/main/java/com/example/babu/jobsandesh/jobseeker/JobseekerLoginActivity.java
@@ -3,37 +3,18 @@ package com.example.babu.jobsandesh.jobseeker;
 
 import android.app.AlertDialog;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.example.babu.jobsandesh.R;
-import com.example.babu.jobsandesh.jstabfragment.BackGroundFragmentActivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 
 public class JobseekerLoginActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     EditText seeker_username,seeker_password;
     Button login_button,button_throw_registerActivity;
-    AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/babu/jobsandesh/tabfragment/OffCampusAdapter.java
@@ -0,0 +1,8 @@
+package com.example.babu.jobsandesh.tabfragment;
+
+/**
+ * Created by Alok on 25/Jan/17.
+ */
+
+public class OffCampusAdapter {
+}
